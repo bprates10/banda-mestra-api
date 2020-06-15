db42de952c2dabca9a5982fa67b02739c9025b65
Bruno Prates <bprates10@gmail.com>
2020-06-15 07:53:37 -0300

    inclusao rota modal

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Event = use('App/Models/Event')
+// const User = use('App/Models/User')
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
@@ -10,17 +11,36 @@ const Event = use('App/Models/Event')
  * Resourceful controller for interacting with events
  */
 class EventController {
+
   // listar todos
   async index({ request }) {
 
-    const { latitude, longitude } = request.all()
+    const { email, latitude, longitude } = request.all()
 
-    const events = Event.query()
-      .with('images')
-      .nearBy(latitude, longitude, 10)
-      .fetch()
+    if (!email) {
+      const events = Event.query()
+        .with('images')
+        .nearBy(latitude, longitude, 10)
+        .fetch()
 
-    return events
+      return events
+    }
+
+    if (email) {
+      // const users = User.query()
+      //   .select('id')
+      //   .where('email', '=', email)
+      //   .fetch()
+
+      // return Object.keys(users)
+      // return JSON.parse(users)
+      const events = Event.query()
+        .with('images')
+        .where('user_id', '=', 1)
+        .fetch()
+
+      return events
+    }
 
   }
 
--- start/routes.js
+++ start/routes.js
@@ -2,10 +2,11 @@
 
 const Route = use('Route')
 
+// criar usuario
 Route.post('/users', 'UserController.create')
-
+// retornar usuario
 Route.get('/users/:mail', 'UserController.show')
-
+// autentica sessao - cria token
 Route.post('/sessions', 'SessionController.create')
 
 Route.resource('events', 'EventController')
